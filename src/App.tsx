6ace801cfcd836e54c38929d293fe6021f1a6338
Nathan Frankel <frankel.nathan@gmail.com>
2026-01-05 15:15:35 -0800

    feat: fetch todays birthdays on button click

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { type ReactNode, useEffect, useState } from "react";
+import { type ReactNode, useCallback, useState } from "react";
 
 import Events, { type Event } from "./components/Events.tsx";
 import { get } from "./utils/http.ts";
@@ -19,32 +19,33 @@ function App() {
   const [isFetching, setIsFetching] = useState(false);
   const [error, setError] = useState<string>();
 
-  useEffect(() => {
-    async function fetchPosts() {
-      setIsFetching(true);
-      try {
-        // TODO: Change to use current date
-        const data = (await get(
-          "https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/births/01/04"
-        )) as ApiResponse;
+  const fetchPosts = useCallback(async () => {
+    setIsFetching(true);
+    setError(undefined);
+    try {
+      const now = new Date();
+      const month = String(now.getMonth() + 1).padStart(2, "0");
+      const day = String(now.getDate()).padStart(2, "0");
 
-        const events: Event[] = data.births.map((rawPost) => {
-          return { text: rawPost.text, year: rawPost.year };
-        });
+      const data = (await get(
+        `https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/births/${month}/${day}`
+      )) as ApiResponse;
 
-        setFetchedEvents(events);
-      } catch (error) {
-        if (error instanceof Error) {
-          setError(error.message);
-        }
+      const events: Event[] = data.births.map((rawPost) => {
+        return { text: rawPost.text, year: rawPost.year };
+      });
+
+      setFetchedEvents(events);
+    } catch (error) {
+      if (error instanceof Error) {
+        setError(error.message);
       }
-      setIsFetching(false);
     }
-
-    fetchPosts();
+    setIsFetching(false);
   }, []);
 
   let content: ReactNode;
+  let buttonContent: ReactNode;
 
   if (error) {
     content = <ErrorMessage text={error} />;
@@ -52,13 +53,22 @@ function App() {
 
   if (fetchedEvents) {
     content = <Events events={fetchedEvents} title="Today's Birthdays" />;
+  } else if (!isFetching) {
+    buttonContent = (
+      <button onClick={fetchPosts}>Fetch Today's Birthdays</button>
+    );
   }
 
   if (isFetching) {
     content = <p>Fetching posts...</p>;
   }
 
-  return <main>{content}</main>;
+  return (
+    <main>
+      {content}
+      {buttonContent}
+    </main>
+  );
 }
 
 export default App;
